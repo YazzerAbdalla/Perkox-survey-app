38d724370edfa3afc5a699d0abb64835bd6e883d
YasserAbdalla <yasserabdalla149@gmail.com>
2024-01-04 20:40:40 +0200

    Edite the background and colors

--- app/Components/Nav-bar.tsx
+++ app/Components/Nav-bar.tsx
@@ -6,19 +6,19 @@ const Navbar = () => {
   return (
     <div className="w-full h-[65px] mb-5 fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-        <a href="/" className="h-auto w-auto flex flex-row items-center">
+        <Link href="/" className="h-auto w-auto flex flex-row items-center">
           <Image
-            src="/NavLogo.png"
+            src="/White-Perkox.png"
             alt="logo"
-            width={70}
-            height={70}
-            className="cursor-pointer hover:animate-slowspin"
+            width={130}
+            height={90}
+            className="cursor-pointer min-w-[130px] mt-2 hover:animate-slowspin"
           />
 
-          <span className="font-bold ml-[10px] uppercase hidden md:block text-gray-800">
+          {/* <span className="font-bold ml-[10px] uppercase hidden md:block text-gray-800">
             prekos
-          </span>
-        </a>
+          </span> */}
+        </Link>
 
         <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-100">
