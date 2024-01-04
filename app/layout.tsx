3d474038c5a884d9bcbd5b7c7d99d6807f7ee595
Youssef Amir <youssefsalib311@gmail.com>
2024-01-04 14:56:17 +0200

    make footer filter

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from "./Components/Nav-bar";
+import Footer from './Components/Footer';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
       <Navbar />
-
+      <Footer />
         {children}
         </body>
     </html>
