23559de12f23f22725140ecf25f2eb51a7cd0729
Youssef Amir <youssefsalib311@gmail.com>
2024-01-04 20:41:49 +0200

    add icon in activities chose devices

--- app/actavities/page.tsx
+++ app/actavities/page.tsx
@@ -1,9 +1,43 @@
 "use client";
+import { useState, useEffect } from 'react';
+import { FaApple, FaDesktop, FaAndroid } from 'react-icons/fa';
+
+type Device = 'iOS' | 'Desktop' | 'Android';
 
 const Activity = () => {
+  const [selectedDevice, setSelectedDevice] = useState<Device | null>(null);
+
+  // useEffect to set the initial selected device
+  useEffect(() => {
+    // Set the initial selected device here (for example, 'Desktop')
+    setSelectedDevice('Desktop');
+  }, []);
+
   return (
     <div>
-      <h1>Activites page</h1>
+      <h1>Activities page</h1>
+      <div>
+        <p>Choose a device:</p>
+        {['iOS', 'Desktop', 'Android'].map((device) => (
+          <button
+            key={device}
+            style={{
+              border: selectedDevice === device ? '3px solid green' : '2px solid transparent',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              padding: '10px', // Add padding for better appearance
+            }}
+            onClick={() => setSelectedDevice(device as Device)}
+          >
+            {device === 'iOS'    &&     <FaApple />}
+            {device === 'Desktop' && <FaDesktop />}
+            {device === 'Android' && <FaAndroid />}
+            {device}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
