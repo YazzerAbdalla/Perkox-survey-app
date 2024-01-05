c5de61cd7cb54c351fc63e0d88cd070fbcc76fdd
YasserAbdalla <yasserabdalla149@gmail.com>
2024-01-05 15:34:07 +0200

    edite the drawer

---
+++ contexts/DeviceContext.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useState, useContext, ReactNode, Dispatch, SetStateAction } from 'react';
+
+interface DeviceContextProps {
+  device: string;
+  setDevice: Dispatch<SetStateAction<string>>;
+}
+
+const DeviceContext = createContext<DeviceContextProps | undefined>(undefined);
+
+interface DeviceProviderProps {
+  children: ReactNode;
+}
+
+export const DeviceProvider: React.FC<DeviceProviderProps> = ({ children }) => {
+  const [device, setDevice] = useState<string>(''); // Set the initial state as needed
+
+  return (
+    <DeviceContext.Provider value={{ device, setDevice }}>
+      {children}
+    </DeviceContext.Provider>
+  );
+};
+
+export const useDevice = (): DeviceContextProps => {
+  const context = useContext(DeviceContext);
+  if (!context) {
+    throw new Error('useDevice must be used within a DeviceProvider');
+  }
+  return context;
+};
