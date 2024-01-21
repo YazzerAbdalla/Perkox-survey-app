b6638ec099bfc340b6a0af758672c771732a35e7
Youssef Amir <youssefsalib311@gmail.com>
2024-01-21 22:04:06 +0200

    edit color of NavBAr

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,10 +30,10 @@ export default function RootLayout({
         <link rel="shortcut icon" href="White-Perkox.ico" type="image/x-icon" />
       </head>
       <body>
-        {/* <div className="max-w-4xl mx-auto p-4 "> */}
+         <div className="max-w-5xl mx-auto p-4 "> 
         <Navbar />
-        {/* </div> */}
-        <div className="mt-20 min-h-[calc(100vh-180px)]">
+         </div> 
+        <div className="mt-17 min-h-[calc(100vh-180px)]">
           <ErrorContextProvider>
             <DataProvider>
               <CardProvider>
