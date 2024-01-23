124a48ff483725547437e10cd874e3078cdcb561
Youssef Amir <youssefsalib311@gmail.com>
2024-01-23 17:13:45 +0200

    fixed the Nav bar active and tern tittle color ,,,

--- app/Components/Nav-bar.tsx
+++ app/Components/Nav-bar.tsx
@@ -2,6 +2,25 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import styled from "styled-components";
+import { colors } from "./theme/Color";
+
+const NavLink = styled(Link)<{ isActive: boolean }>`
+  color: ${({ isActive }) => (isActive ? colors.palette.purple : colors.palette.neutral100)};
+
+  &:hover,
+  &:active {
+    color: ${colors.palette.purple};
+    border-bottom-color: rgb(194, 0, 248);
+  }
+`;
+
+const routes = {
+  home: "/",
+  activities: "/activities",
+  term: "/term-of-service",
+};
 
 const Navbar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -10,32 +29,43 @@ const Navbar = () => {
     setMenuOpen(!isMenuOpen);
   };
 
+  const pathname = usePathname();
+
   return (
     <header>
-      
-       <Link href="/" >
-      <div className="logo"> <Image
-            src="/White-Perkox.png"  
+      <Link href="/">
+        <div className="logo">
+          <Image
+            src="/White-Perkox.png"
             alt="White-Perkox"
-            width={150}  
-            height={60}  
-          /></div>
+            width={150}
+            height={60}
+          />
+        </div>
       </Link>
 
       <input type="checkbox" id="nav_check" hidden />
-      <nav className="z-[1000]">
+      <nav>
         <ul>
           <li>
-            <Link href="/" className="active">
+            <NavLink isActive={pathname === routes.home} href={routes.home}>
               Home
-            </Link>
+            </NavLink>
           </li>
 
           <li>
-            <Link href="activities">Activity</Link>
+            <NavLink
+              isActive={pathname === routes.activities}
+              href={routes.activities}
+            >
+              Activity
+            </NavLink>
           </li>
+
           <li>
-            <Link href="term-of-service">terms of service</Link>
+            <NavLink isActive={pathname === routes.term} href={routes.term}>
+              Term of service
+            </NavLink>
           </li>
         </ul>
       </nav>
