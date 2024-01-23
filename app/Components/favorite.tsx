3faff86a5501cc110e6157368f76e367e79f9949
YasserAbdalla <yasserabdalla149@gmail.com>
2024-01-23 20:46:33 +0200

    change the z-index of favorite

--- app/Components/favorite.tsx
+++ app/Components/favorite.tsx
@@ -4,7 +4,7 @@
 
 export default function Favourite() {
   return (
-    <div className="max-w-5xl mx-auto p-14 mt-10 border-2 border-purple-900 my-3 flex justify-between gap-5 relative ">
+    <div className="max-w-5xl mx-auto z-[10] p-14 mt-10 border-2 border-purple-900 my-3 flex justify-between gap-5 relative ">
       <span className="bg-purple-900 text-white px-6 py-1 rounded-full  text-md font-semiblod whitespace-nowrap absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         Favorite🎇
       </span>
