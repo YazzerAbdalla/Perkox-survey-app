ed37d912ff3de83c86a4fc42406085b9effb8712
Youssef Amir <youssefsalib311@gmail.com>
2024-01-25 22:24:39 +0200

    add icon in nav ,,,, edit color in status

--- app/Components/ActivitiesCard.tsx
+++ app/Components/ActivitiesCard.tsx
@@ -1,31 +1,54 @@
 import axios from "axios";
 import Image from "next/image";
 import React from "react";
+
 interface ActivitiesCardProps {
   offername: string;
   status: string;
 }
+
 const ActivitiesCard = ({ offername, status }: ActivitiesCardProps) => {
+  const getStatusColor = () => {
+    if (status === "pending" &&"Pending") {
+      return "#FFFF00"; 
+    } else if (status === "completed" && "Completed") {
+      return "#b7f763"; 
+    }
+    return "#fa0000"; 
+  };
+
+  const cardStyle = {
+    width: "20rem",
+    borderRadius: "1.5rem",
+    height: "10rem",
+    padding: "1rem",
+  };
+
+  const statusTextStyle = {
+    width: "fit-content",
+    padding: "0.5rem 1rem",
+    borderRadius: "18px",
+    marginLeft: "-5px",
+    color: "#0a1f59",
+    fontWeight: "bold",
+    backgroundColor: getStatusColor(), 
+  };
+
   return (
-    <>
-      <div className="w-[20rem]  rounded-[1.5rem] h-[10rem] activities-card p-5">
-        <div className="flex mt-4 row-auto gap-3 items-center text-white">
-          {/* <Image
-            src={
-              "/_next/image?url=https%3A%2F%2Fmedia.go2speed.org%2Fbrand%2Ffiles%2Fogmobi%2F48045%2Fthumbnails_100%2F1200x600wa.png&w=128&q=75"
-            }
-            width={60}
-            height={60}
-            alt="img"
-            className="rounded-[12px] object-cover"
-          /> */}
-          <h2 className="truncate">{offername}</h2>
-        </div>
-        <h4 className="mt-3 w-fit bg-[#b7f763] py-2 px-3 rounded-[18px] ml-[-5px] text-[#0a1f59] font-semibold ">
-          {status}
-        </h4>
+    <div style={cardStyle} className="rounded-[1.5rem] h-[10rem] activities-card p-5">
+      <div className="flex mt-4 row-auto gap-3 items-center text-white">
+        {/* Add your Image component here */}
+        <h2 className="truncate">{offername}</h2>
       </div>
-    </>
+      <h4 style={statusTextStyle} className="mt-5">
+        {status && (
+          <>
+            <span style={{ textTransform: 'capitalize' }}>{status.charAt(0)}</span>
+            {status.slice(1)}
+          </>
+        )}
+      </h4>
+    </div>
   );
 };
 
