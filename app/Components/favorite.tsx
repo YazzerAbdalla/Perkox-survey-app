35164d207aa968048ebdb1b25ac999f8f65d4942
YasserAbdalla <yasserabdalla149@gmail.com>
2024-01-25 23:20:55 +0200

    Making the favorite section work

--- app/Components/favorite.tsx
+++ app/Components/favorite.tsx
@@ -1,13 +1,33 @@
 "use client";
 
-// p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start
+import { Offer } from "@/contexts/DataContext";
+import DrawerDemo from "./Drawer";
+import { dataProps } from "../page";
+import { useState } from "react";
 
-export default function Favourite() {
+// p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start
+interface favProps {
+  fav: Offer[] | null;
+}
+export default function Favourite({ fav }: favProps) {
+  const [selectedCardID, setSelectedCardID] = useState<number | null>(null);
   return (
-    <div className="max-w-5xl mx-auto z-[10] p-14 mt-10 border-2 border-purple-900 my-3 flex justify-between gap-5 relative ">
+    <div className="max-w-5xl mx-auto z-[10] p-8 mt-10 border-2 border-purple-900 my-3 gap-1 relative md:grid md:grid-cols-2">
       <span className="bg-purple-900 text-white px-6 py-1 rounded-full  text-md font-semiblod whitespace-nowrap absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         Favorite🎇
       </span>
+      {fav?.map(({ id, name, instructions, image, reward, os }: dataProps) => (
+        <DrawerDemo
+          key={id}
+          name={name}
+          instructions={instructions}
+          image={image}
+          reward={reward}
+          os={os}
+          id={id}
+          onClick={() => setSelectedCardID(id)} // Set the selected card ID on click
+        />
+      ))}
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import { useFilteredDataContext } from "@/contexts/FilteredDataContext";
 import Favorite from "./Components/favorite";
 import NOoffer from "./Components/No-offer";
 
-interface dataProps {
+export interface dataProps {
   name: string;
   instructions: string;
   image: string;
@@ -26,7 +26,9 @@ export default function Home() {
   const { filteredDataArr, setFilteredDataArr } = useFilteredDataContext();
   const { error, setError } = useErrorContext();
   const { filter, setFilter } = useFilter();
-  const [selectedCardID, setSelectedCardID] = useState<number | null>(null); // Track selected card ID
+  const [selectedCardID, setSelectedCardID] = useState<number | null>(null);
+  const [fav, setFav] = useState<Offer[] | []>([]);
+  // Track selected card ID
 
   // Add more items as needed
 
@@ -34,6 +36,8 @@ export default function Home() {
     //@ts-ignore
     // Set the dataArr once the data is fetched
     fetchData(setFilteredDataArr, setDataArr, setError, filter);
+    let theFavCards = dataArr.filter((item) => item.favorite === 1);
+    setFav(theFavCards);
   }, []);
   useEffect(() => {
     let filteredData = dataArr.filter((item: Offer) => item.model === filter);
@@ -44,25 +48,33 @@ export default function Home() {
     <>
       <OfferFilter setFilter={setFilter} />
       <ButtonFilter />
-      <Favorite />
-      {/* <NOoffer /> */}
- 
+      {fav.length !== 0 && (
+        <>
+          <Favorite fav={fav} />
+        </>
+      )}
       <div className="flex  flex-col md:flex-row gap-0  content-center flex-wrap md:px-20 px-4 mt-7 w-full">
-        {filteredDataArr.map(
-          ({ id, name, instructions, image, reward, os }: dataProps) => (
-            <DrawerDemo
-              key={id}
-              name={name}
-              instructions={instructions}
-              image={image}
-              reward={reward}
-              os={os}
-              id={id}
-              onClick={() => setSelectedCardID(id)} // Set the selected card ID on click
-            />
+        {filteredDataArr.length === 0 ? (
+          <div className="w-full flex justify-center">
+            <NOoffer />
+          </div>
+        ) : (
+          filteredDataArr.map(
+            ({ id, name, instructions, image, reward, os }: dataProps) => (
+              <DrawerDemo
+                key={id}
+                name={name}
+                instructions={instructions}
+                image={image}
+                reward={reward}
+                os={os}
+                id={id}
+                onClick={() => setSelectedCardID(id)} // Set the selected card ID on click
+              />
+            )
           )
         )}
-      </div> 
+      </div>
     </>
   );
 }
