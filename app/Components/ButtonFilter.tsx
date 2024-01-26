cd2e7f4cbcd545905167b2b514c6b411773f8ee2
YasserAbdalla <yasserabdalla149@gmail.com>
2024-01-26 15:47:11 +0200

    Make the platform sort function working

--- app/Components/ButtonFilter.tsx
+++ app/Components/ButtonFilter.tsx
@@ -1,29 +1,57 @@
 "use client";
-import { Fragment, useState } from "react";
+import { Dispatch, Fragment, SetStateAction, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 
-const people = [
+export const sortOptions = [
+  { name: "All" },
   { name: "Most Popular" },
-  { name: "ios" },
   { name: "Highest Paying" },
 ];
+export const platforms = [
+  { name: "All" },
+  { name: "iOS" },
+  { name: "Android" },
+  { name: "Web" },
+];
+interface sortProps {
+  selectedSort: {
+    name: string;
+  };
+  selectedPlatform: {
+    name: string;
+  };
+  setSelectedSort: Dispatch<
+    SetStateAction<{
+      name: string;
+    }>
+  >;
+  setSelectedPlatform: Dispatch<
+    SetStateAction<{
+      name: string;
+    }>
+  >;
+}
 
-export default function Button() {
-  const [selected, setSelected] = useState(people[0]);
-
+export default function Button({
+  selectedSort,
+  setSelectedSort,
+  selectedPlatform,
+  setSelectedPlatform,
+}: sortProps) {
   return (
     <div className="flex items-center z-[10] justify-center space-x-4  ">
       {/* First dropdown */}
-      <Listbox value={selected} onChange={setSelected}>
-        <div className="relative mt-1">
-          <Listbox.Button 
-          className="relative w-full cursor-default rounded-lg text-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
-          style={{
-            background: 'linear-gradient(150deg, rgb(84 1 141) 0%, rgba(89, 42, 102, 1) 99%)',
-          }}
+      <Listbox value={selectedSort} onChange={setSelectedSort}>
+        <div className="relative mt-1 z-[100] min-w-[7rem]">
+          <Listbox.Button
+            className="relative w-full cursor-default rounded-lg text-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+            style={{
+              background:
+                "linear-gradient(150deg, rgb(84 1 141) 0%, rgba(89, 42, 102, 1) 99%)",
+            }}
           >
-            <span className="block truncate">{selected.name}</span>
+            <span className="block truncate">{selectedSort.name}</span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon
                 className="h-5 w-5 text-white"
@@ -38,15 +66,15 @@ export default function Button() {
             leaveTo="opacity-0"
           >
             <Listbox.Options className="absolute z-[1000] top-0 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-              {people.map((person, personIdx) => (
+              {sortOptions.map((option, optionIdx) => (
                 <Listbox.Option
-                  key={personIdx}
+                  key={optionIdx}
                   className={({ active }) =>
                     `relative cursor-default select-none py-2 pl-10 pr-4 ${
                       active ? "bg-amber-100 text-black" : "text-black"
                     }`
                   }
-                  value={person}
+                  value={option}
                 >
                   {({ selected }) => (
                     <>
@@ -55,7 +83,7 @@ export default function Button() {
                           selected ? "font-medium" : "font-normal"
                         }`}
                       >
-                        {person.name}
+                        {option.name}
                       </span>
                       {selected ? (
                         <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-purple-900 font-solid">
@@ -71,17 +99,17 @@ export default function Button() {
         </div>
       </Listbox>
 
-      {/* Second dropdown, same adjustments */}
-      <Listbox value={selected} onChange={setSelected}>
-        <div className="relative mt-1">
-          <Listbox.Button 
-          className="relative w-full cursor-default rounded-lg text-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
-          style={{
-            background: 'linear-gradient(150deg, rgb(84 1 141) 0%, rgba(89, 42, 102, 1) 99%)',
-          }}
-          
+      {/* Second dropdown */}
+      <Listbox value={selectedPlatform} onChange={setSelectedPlatform}>
+        <div className="relative mt-1 z-[100] min-w-[7rem]">
+          <Listbox.Button
+            className="relative w-full cursor-default rounded-lg text-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+            style={{
+              background:
+                "linear-gradient(150deg, rgb(84 1 141) 0%, rgba(89, 42, 102, 1) 99%)",
+            }}
           >
-            <span className="block truncate">{selected.name}</span>
+            <span className="block truncate">{selectedPlatform.name}</span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon
                 className="h-5 w-5 text-white"
@@ -96,15 +124,15 @@ export default function Button() {
             leaveTo="opacity-0"
           >
             <Listbox.Options className="absolute z-[1000] top-0 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-              {people.map((person, personIdx) => (
+              {platforms.map((option, optionIdx) => (
                 <Listbox.Option
-                  key={personIdx}
+                  key={optionIdx}
                   className={({ active }) =>
                     `relative cursor-default select-none py-2 pl-10 pr-4 ${
                       active ? "bg-amber-100 text-purple-900" : "text-gray-900"
                     }`
                   }
-                  value={person}
+                  value={option}
                 >
                   {({ selected }) => (
                     <>
@@ -113,7 +141,7 @@ export default function Button() {
                           selected ? "font-medium" : "font-normal"
                         }`}
                       >
-                        {person.name}
+                        {option.name}
                       </span>
                       {selected ? (
                         <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-purple-900 font-solid">
