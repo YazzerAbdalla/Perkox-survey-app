0c464b2582d0fe840d78366995eb0bf8136fce82
Youssef Amir <youssefsalib311@gmail.com>
2024-01-26 21:40:46 +0200

    Nav bar after edit

--- app/Components/Nav-bar.tsx
+++ app/Components/Nav-bar.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { navData } from "./data";
 import Image from "next/image";
+import { BsList } from "react-icons/bs";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -14,13 +15,17 @@ const Navbar = () => {
   };
 
   return (
-  <div className="w-full z-[1500] items-center">
+    <div className="w-full z-[1500] items-center">
       {/* Navbar container */}
       <nav
         className="fixed top-0 left-0 right-0 z-[1500] md:flex justify-between md:px-10 px-7"
         style={{
           background:
             "linear-gradient(150deg, rgb(84 1 141) 0%, rgba(89, 42, 102, 1) 99%)",
+          height: "70px", // Set height to 70px
+          display: "flex",
+          alignItems: "center",
+          padding: "0 50px",
         }}
       >
         {/* Logo on the left */}
@@ -31,41 +36,31 @@ const Navbar = () => {
               alt="White-Perkox"
               width={150}
               height={60}
+              className="font-size: 30px; text-transform: uppercase; color: white; background-image: url(/White-Perkox.png);"
             />
           </Link>
         </div>
 
-        <div className=" z-[1500] text-2xl absolute right-2 top-8 cursor-pointer md:hidden">
+        <div className="z-[1500] text-2xl absolute right-2 top-8 cursor-pointer md:hidden">
           <button
-            className="focus:outline-none w-8 h-8 overflow-hidden "
+            className="focus:outline-none w-8 h-8 overflow-hidden flex items-center justify-center"
             onClick={handleClick}
           >
-            <span
-              className={`bg-white transition-all duration-300 ease-in-out  block h-0.5 w-6 ${
+            {/* Menu Icon using react-icons */}
+            <BsList
+              className={`text-white transition-all duration-300 ease-in-out block h-6 w-6 ${
                 isOpen ? "rotate-45 translate-y-1" : "-translate-y-1"
               }`}
-            ></span>
-            <span
-              className={`bg-white transition-all duration-300 ease-in-out  block h-0.5 w-6  my-0.5 ${
-                isOpen
-                  ? "-translate-x-5 opacity-0"
-                  : "translate-x-0  opacity-100"
-              }`}
-            ></span>
-            <span
-              className={`bg-white transition-all duration-300 ease-in-out  block h-0.5 w-6 rounded-sm  ${
-                isOpen ? " -rotate-45 -translate-y-1" : "translate-y-1"
-              } `}
-            ></span>
+            />
           </button>
         </div>
 
         <ul
-           style={{
+          style={{
             background:
               "linear-gradient(150deg, rgb(84 1 141) 0%, rgba(89, 42, 102, 1) 99%)",
           }}
-          className={`w-full    pb-12 absolute   md:flex md:items-center md:pb-0 md:static  z-1 md:z-auto  md:w-auto md:pl-0 pl-9 transition-all duration-500 ease top-20 ${
+          className={`w-full pb-12 absolute md:flex md:items-center md:pb-0 md:static z-1 md:z-auto md:w-auto md:pl-0 pl-9 transition-all duration-500 ease top-20 ${
             isOpen ? " left-0" : " -left-full "
           }`}
         >
@@ -77,13 +72,13 @@ const Navbar = () => {
             >
               <Link
                 href={link.url}
-                className={`  ${
+                className={`${
                   currentRoute === link.url
-                  ? "text-purple-500 border-b-2 border-purple-500"
-                  : "text-white hover:text-gray-300 hover:border-purple-500 border-b-2 border-transparent "
-              }`}
+                    ? "text-purple-500 border-b-2 border-purple-500"
+                    : "text-white hover:text-gray-300 hover:border-purple-500 border-b-2 border-transparent "
+                }`}
               >
-                {link.title}{" "}
+                {link.title}
               </Link>
             </li>
           ))}
