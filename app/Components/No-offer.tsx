5e856feaee7c46bea0780bd29f4f8fcddf380f57
Youssef Amir <youssefsalib311@gmail.com>
2024-01-27 18:10:42 +0200

    change card nofoofer and if no action

--- app/Components/No-offer.tsx
+++ app/Components/No-offer.tsx
@@ -4,24 +4,20 @@ import Image from "next/image";
 
 export default function NoOffer() {
   return (
-    <div className="flex justify-center items-center">
-      <div className="card " style={{ outline: "1px solid #6B46C1" }}>
-        <Image
-          src="/image_about_no_offers_yet_like_icon_i_want_to_pu.jpg"
-          alt="White-Perkox"
-          width={100}
-          height={350}
-          layout="responsive"
-        />
-        <div className="card-content">
-          <h2
-            className="flex justify-center items-center text-white mt-6"
-            style={{ outline: "4px solid #8369be" }}
-          >
-            Sorry, No offers yet
-          </h2>
-        </div>
-      </div>
-    </div>
-  );
+    <div className="pt-10 xl:pt-20 items-center justify-center">
+    <Image
+      src="/Asset_1.svg"
+      alt="hero image"
+      width={1025}
+      height={500}
+      className="flex items-center justify-center mx-auto w-80 xl:w-120" 
+    />
+    <h2
+      className="flex justify-center items-center text-white mt-6"
+      style={{ outline: "4px solid #8369be" }}
+    >
+      Sorry, No offers yet
+    </h2>
+  </div>
+  );  
 }
