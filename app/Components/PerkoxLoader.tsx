3119c884d692e021c6a5f53559118db0f675473d
Youssef Amir <youssefsalib311@gmail.com>
2024-01-27 20:33:22 +0200

    add eidt in preloading

--- app/Components/PerkoxLoader.tsx
+++ app/Components/PerkoxLoader.tsx
@@ -3,9 +3,12 @@ import Image from "next/image";
 
 const PerkoxLoader = () => {
   return (
-    <div className="container">
+   <div className="bodyLoading">
+   <div className="container">
       <div className="lds-dual-ring"></div>
     </div>
+    </div>
+
   );
 };
 
