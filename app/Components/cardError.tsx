3e07c099f14b0c4131f99b5e9f8e2c4494331bcf
YasserAbdalla <yasserabdalla149@gmail.com>
2024-01-27 20:22:04 +0200

    Edite the error image

--- app/Components/cardError.tsx
+++ app/Components/cardError.tsx
@@ -6,24 +6,23 @@ interface ErrorProps {
 }
 export default function NoOffer({ error }: ErrorProps) {
   return (
-    <div className="pt-10 xl:pt-20 items-center justify-center">
-    <Image
-      src="/Asset_2.svg"
-      alt="hero image"
-      width={1025}
-      height={500}
-      className="flex items-center justify-center mx-auto w-80 lg:w-96" 
+    <div className="pt-10 xl:pt-20 flex flex-col items-center justify-center">
+      <Image
+        src="/Asset_2.svg"
+        alt="hero image"
+        width={1025}
+        height={500}
+        className="flex items-center justify-center mx-auto w-80 lg:w-96"
       />
-    <h2
-      className="flex justify-center items-center  mt-6"
-      style={{ outline: "4px solid #8369be"  , color:"#D53F3F"}}
-    >
-      Something went wrong...<br/>
-      {error}
-    </h2>
- 
-  </div>
-    
-  
+      <div
+        className="flex justify-center mt-3 px-24 flex-col items-center "
+        style={{ outline: "4px solid #8369be" }}
+      >
+        <h2 className=" " style={{ color: "#D53F3F" }}>
+          Something went wrong...
+        </h2>
+        <h2 className="font-bold" style={{ color: "#D53F3F" }}>{error}</h2>
+      </div>
+    </div>
   );
 }
