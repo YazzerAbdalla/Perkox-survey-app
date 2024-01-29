20ee7f889629449a03a36e4e03a1dbefd0080784
Youssef Amir <youssefsalib311@gmail.com>
2024-01-29 19:28:51 +0200

    change status place and add image i=not loading

--- app/Components/ActivitiesCard.tsx
+++ app/Components/ActivitiesCard.tsx
@@ -5,16 +5,18 @@ import React from "react";
 interface ActivitiesCardProps {
   offername: string;
   status: string;
+  image: string;
+
 }
 
-const ActivitiesCard = ({ offername, status }: ActivitiesCardProps) => {
+const ActivitiesCard = ({ offername, status, image }: ActivitiesCardProps) => {
   const getStatusColor = () => {
-    if (status === "pending" &&"Pending") {
-      return "#EDC55E"; 
+    if (status === "pending" && "Pending") {
+      return "#EDC55E";
     } else if (status === "completed" && "Completed") {
-      return "#409977"; 
+      return "#409977";
     }
-    return "#D53F3F"; 
+    return "#D53F3F";
   };
 
   const cardStyle = {
@@ -28,16 +30,17 @@ const ActivitiesCard = ({ offername, status }: ActivitiesCardProps) => {
     width: "fit-content",
     padding: "0.5rem 1rem",
     borderRadius: "18px",
-    marginLeft: "-5px",
-    color: "#000000" ,
+    marginLeft: "180px",
+    color: "#000000",
     fontWeight: "solid",
-    backgroundColor: getStatusColor(), 
+    backgroundColor: getStatusColor(),
   };
 
   return (
-    <div style={cardStyle} className="rounded-[1.5rem] h-[10rem] activities-card p-5">
-      <div className="flex mt-4 row-auto gap-3 items-center text-white">
+    <div style={cardStyle} className="rounded-[1.5rem] mt-6 h-[10rem] activities-card p-5">
+      <div className="flex mt-1 row-auto gap-3 items-center text-white">
         {/* Add your Image component here */}
+        <Image src={image} width={60} height={60} alt="" loading="lazy" />
         <h2 className="truncate">{offername}</h2>
       </div>
       <h4 style={statusTextStyle} className="mt-5">
